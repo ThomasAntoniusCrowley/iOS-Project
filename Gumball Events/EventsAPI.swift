692efdd68b155b7ba5d1bbc4a0005eb4b4c9565b
Thomas Crowley <sc14talc@leeds.ac.uk>
2016-12-03 17:37:14 +0000

    Successfully established connection with Events API and can now pull events data

--- Gumball Events/EventsAPI.swift
+++ Gumball Events/EventsAPI.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+
+let key = "2Qm4LQs466gpMpnV"
+
+
+//func postKey(){
+//    
+//
+//
+//var request = URLRequest(url: URL(string: "http://api.eventful.com/json/events/search?app_key=2Qm4LQs466gpMpnV&keywords=books&location=San+Diego&date=Futureo")!)
+////request.httpMethod = "POST"
+////let postString = key
+////request.httpBody = postString.data(using: .utf8)
+//let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//    guard let data = data, error == nil else {                                                 // check for fundamental networking error
+//        print("error=\(error)")
+//        return
+//    }
+//    
+//    if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+//        print("statusCode should be 200, but is \(httpStatus.statusCode)")
+//        print("response = \(response)")
+//    }
+//    
+//    let responseString = String(data: data, encoding: .utf8)
+//    print("responseString = \(responseString)")
+//}
+//task.resume()
+//}
+
+
+
+func postKey()
+
+{
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+    let baseURL = URL(string: "http://api.eventful.com/json/events/search?app_key=2Qm4LQs466gpMpnV&keywords=books&location=San+Diego&date=Futureo")!
+    print("Set up session")
+    
+    let eventTask = session.dataTask(with: baseURL, completionHandler: {
+        (body, response, error) in
+        
+        print(response)
+        if error != nil {
+            
+            print("Error: " + error!.localizedDescription)
+        } else {
+            
+            do {
+                
+                if let json = try JSONSerialization.jsonObject(with: body!, options: .allowFragments) as? [String: Any] {
+                    print(json)
+                }
+            } catch {
+                
+                print("error in JSONSerialization")
+            }
+        }
+    })
+    eventTask.resume()
+}
+
