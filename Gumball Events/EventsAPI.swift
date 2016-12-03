6d6760bde97517f69f1147310cfbd0539e95cf95
Thomas Crowley <sc14talc@leeds.ac.uk>
2016-12-03 16:07:49 +0000

    file created to Handle events API

---
+++ Gumball Events/EventsAPI.swift
@@ -0,0 +1,9 @@
+//
+//  EventsAPI.swift
+//  Gumball Events
+//
+//  Created by Thomas Crowley [sc14talc] on 03/12/2016.
+//  Copyright © 2016 Thomas Crowley [sc14talc]. All rights reserved.
+//
+
+import Foundation
